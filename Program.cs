58901ab551497d38e6e2c6b8ac9b5476ec1d3a6a
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-13 01:26:09 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("O que você pensa de mim? ");
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("");
+Console.WriteLine("- E sabe o que eu penso de você?");
+Console.WriteLine($"{texto.Remove(10)}... NÃO, PERA.");
+Console.WriteLine("Tô nervoso");
